676d993d5c02b3815dd9e26ca2ce4b1a29cec909
Antoine Leblanc <crucuny@gmail.com>
2025-02-21 21:27:16 +0000

    Minor cleanups.

--- test/Tests.hs
+++ test/Tests.hs
@@ -9,9 +9,17 @@ custom_main = True
 
 import "this" Prelude
 
+import Data.List                 (isPrefixOf)
+import System.Environment
 import Test.Tasty
+import Test.Tasty.Runners.AntXML
 
 {- AUTOCOLLECT.MAIN.imports -}
 
 main :: IO ()
-main = defaultMain $ testGroup "graphwiz" {- AUTOCOLLECT.MAIN.tests -}
+main = do
+  args <- getArgs
+  let tests = testGroup "graphwiz" ({- AUTOCOLLECT.MAIN.tests -})
+  if any ("--xml" `isPrefixOf`) args
+  then defaultMainWithIngredients [antXMLRunner] tests
+  else defaultMain tests
