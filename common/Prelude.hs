3b1a5461dcacdea0d318a2bcf51c02de5b4f17d1
Antoine Leblanc <crucuny@gmail.com>
2025-12-13 17:10:11 +0000

    Remove unused Prelude functions.

--- common/Prelude.hs
+++ common/Prelude.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-orphans #-}
-{-# LANGUAGE PatternSynonyms #-}
-
 {- |
 
 Internal module that re-exports the regular prelude, plus some common useful
@@ -8,23 +5,7 @@ functions.
 
 -}
 
-module Prelude
-  ( -- * re-exports from useful "default" modules
-    module P
-    -- * custom operators
-  , (...)
-    -- * maybe helpers
-  , onNothing
-  , onNothingM
-    -- * either helpers
-  , onLeft
-  , onLeftM
-    -- * sequence helpers
-  , Seq (.., Lone)
-    -- * hashmap helpers
-  , unionWithM
-  , unionWithKeyM
-  ) where
+module Prelude (module P) where
 
 
 -- re-exports
@@ -65,65 +46,3 @@ import Data.Traversable           as P (for)
 import Data.Void                  as P (Void, absurd)
 import GHC.Generics               as P (Generic)
 import "base" Prelude             as P hiding (lookup)
-
-
--- internal imports
-
-import Data.HashMap.Strict        qualified as M
-import Data.Sequence              (Seq (..))
-
-
--- operators
-
-(...) :: (c -> d) -> (a -> b -> c) -> (a -> b -> d)
-f ... g = \x y -> f (g x y)
-infixr 8 ...
-
-
--- maybe helpers
-
-onNothing :: Applicative m => Maybe a -> m a -> m a
-onNothing a d = maybe d pure a
-
-onNothingM :: Monad m => m (Maybe a) -> m a -> m a
-onNothingM a d = a >>= flip onNothing d
-
-
--- either helpers
-
-onLeft :: Applicative m => Either e a -> (e -> m a) -> m a
-onLeft a f = either f pure a
-
-onLeftM :: Monad m => m (Either e a) -> (e -> m a) -> m a
-onLeftM a f = a >>= flip onLeft f
-
-
--- sequence helpers
-
-pattern Lone :: a -> Seq a
-pattern Lone x = x :<| Empty
-
-
--- hashmap helpers
-
-unionWithM ::
-  (Monad m, Hashable k) =>
-  (v -> v -> m v) ->
-  HashMap k v ->
-  HashMap k v ->
-  m (HashMap k v)
-unionWithM = unionWithKeyM . const
-
-unionWithKeyM ::
-  (Monad m, Hashable k) =>
-  (k -> v -> v -> m v) ->
-  HashMap k v ->
-  HashMap k v ->
-  m (HashMap k v)
-unionWithKeyM f m1 m2 = foldM step m1 (M.toList m2)
-  where
-    step m (k, new) = case M.lookup k m of
-      Nothing -> pure $ M.insert k new m
-      Just old -> do
-        combined <- f k new old
-        pure $ M.insert k combined m
